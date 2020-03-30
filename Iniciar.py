6f8e10a872d08b40122007411eed127a8d8daceb
Julia <juliarexsteam@hotmail.com>
2020-03-30 17:04:59 -0300

    Inicial do projeto

---
+++ Iniciar.py
@@ -0,0 +1,29 @@
+while True:
+    print("Seja Bem-Vindo !")
+    print("Acessos:\n 1 - Professor\n 2 - Alunos\n 3 - Disciplina\n 4 - Turmas\n 5 - Sair")
+    acesso = (input("Digite a operação que deseja acessar:"))
+    if acesso == "1":
+        while True:
+            print("Acessos:\n 1 -> Cadastrar Professor(a)\n 2 -> Atualizar Informação\n 3 -> Consultar Professor(a)\n 4 -> Deletar Professor(a)\n 5 -> Sair")
+            prof = (input("Digite a operação que deseja realizar:"))
+
+
+            
+     if acesso == "2":
+        while True:
+            print("Acessos:\n 1 -> Cadastrar Aluno(a)\n 2 -> Atualizar Informação\n 3 -> Consultar Aluno(a)\n 4 -> Deletar Aluno(a)\n 5 -> Sair")
+            aluno = (input("Digite a operação que deseja realizar:"))
+
+
+
+    if acesso == "3":
+        while True:
+            print("Acessos:\n 1 -> Cadastrar Disciplina\n 2 -> Atualizar Informação\n 3 -> Consultar Disciplina\n 4 -> Deletar Disciplina\n 5 -> Sair")
+            disciplina = (input("Digite a operação que deseja realizar:"))
+
+
+    if acesso == "4":
+        while True:
+            print("Acessos:\n 1 -> Cadastrar Turma\n 2 -> Atualizar Informação\n 3 -> Consultar Turma\n 4 -> Deletar Turma\n 5 -> Gerar Ata\n 6 -> Sair")
+            turma = (input("Digite a operação que deseja realizar:"))
+    
