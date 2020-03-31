af52877edb0ec47c5163c8ddec16412fab59df1d
Julia <juliarexsteam@hotmail.com>
2020-03-31 12:57:47 -0300

    Cadastro aluno e disciplina adicionados

--- Cadastros.py
+++ Cadastros.py
@@ -16,5 +16,42 @@ def cadastroprofessor():
     arq.write("\n")
     arq.close()
 
-cadastroprofessor()
+def cadastroaluno():
+    arq = open("aluno.txt","a")
+    nome = input("Digite o nome do novo aluno(a): ")
+    arq.write(nome)
+    arq.write(" | ")
+    curso = input("Digite o curso do novo aluno(a): ")
+    arq.write(curso)
+    arq.write(" | ")
+    while True:
+        cpf = input("Digite o cpf do novo aluno(a): ")
+        if cpf.isnumeric and len(cpf) == 11:
+            arq.write(cpf)
+            break
+        else:
+            print("CPF invalido")
+    arq.write("\n")
+    arq.close
+
+def cadastrodisciplina():
+    arq = open("disciplina.txt","a")
+    disciplina = input("Digite o nome da nova disciplina: ")
+    arq.write(disciplina)
+    arq.write(" | ")
+    curso = input("Digite o curso onde vai adicionar a nova disciplina: ")
+    arq.write(curso)
+    arq.write(" | ")
+    while True:
+        codigo = input("Digite um código novo para a disciplina(4 números): ")
+        if codigo.isnumeric and len(codigo)==4:
+            arq.write(codigo)
+            break
+        else:
+            ("Codigo Invalido")
+    arq.write("\n")
+    arq.close
+
+
+    
         
